01ed8dcb6c8937be14582456ee8a4077ccb5200a
hemangdtu <hemang.dtu@gmail.com>
2021-02-28 20:56:18 +0530

    Clock layout completed

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:clock_app/clockView.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Clock App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        color: Color(0XFF2D2F41),
+        child: ClockView(),
+      ),
+    );
+  }
+}
